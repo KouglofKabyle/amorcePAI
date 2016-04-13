068911fc4989d3e85bd45c1844223f5cb3fc1e4c
KouglofKabyle <yoann.ouab@gmail.com>
2016-04-13 16:41:22 +0200

    Up now

--- js/secretary.js
+++ js/secretary.js
@@ -3,11 +3,9 @@ var angular = require("angular"), angularMaterial=require( "angular-material" );
 
 require( "angular-material/angular-material.css" );
 
-var cabinetModule = angular.module( "cabinet", [ angularMaterial, angularIcons ] )
+var cabinetModule = angular.module( "cabinet", [ angularMaterial ] )
 		.config(function($mdThemingProvider) {
   			$mdThemingProvider.theme('default');
 		});;
 
 require( "./cabinetMedical/cabinetMedical.js" )(cabinetModule);
-
-
